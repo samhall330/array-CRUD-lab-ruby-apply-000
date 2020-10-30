ee7c7e6bf052f2b45d3840d85ef3f590604bd167
Sam Hall <mann.samantha11@gmail.com>
2020-10-30 00:19:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  index_number[2]
+  index_number = array[2]
   index_number
 end
 
